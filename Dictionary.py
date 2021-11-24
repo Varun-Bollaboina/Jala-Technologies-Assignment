d9559c02a3085b56b258a5cdcfde9ebe3e3fe8e0
Varun-Bollaboina <94756305+Varun-Bollaboina@users.noreply.github.com>
2021-11-24 16:14:12 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,15 @@
+#Dictionary
+
+#Adding the values in dictionary
+#Updating the values in dictionary
+#Accessing the value in dictionary
+#Create a nested loop dictionary
+#Access the values of nested loop dictionary
+#Print the keys present in a particular dictionary
+
+
+
+studentID = {1: {'id 472': 'varun', 'age': '23', 'sex': 'Male'},
+          2: {'id 293': 'shivapriya', 'age': '22', 'sex': 'Female'}}
+
+print(studentID)
