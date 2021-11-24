bf15001d60c50720f6a3301485a92fd43cff6a16
Varun-Bollaboina <94756305+Varun-Bollaboina@users.noreply.github.com>
2021-11-24 16:41:30 +0530

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,18 @@
+#Inheritance
+
+#Create three methods in each class, 2 methods are specific to each class and third method (override method) should be in all three Classes A, B and C
+#Call an overridden method with super class reference to B and C class’s objects
+
+
+class A:
+    def m(self):
+        print("m of A called")
+class B(A):
+    pass
+class C(A):
+    def m(self):
+        print("m of C called")
+class A(B,C):
+    pass
+x = A()
+x.m()
